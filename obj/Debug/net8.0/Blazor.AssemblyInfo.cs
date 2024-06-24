957d20c2efbffd055ef6d145c338979a6cdfa9e4
Utkarsh Vishnoi <utkarsh.v8274@gmail.com>
2024-06-25 01:38:40 +0530

    Added more structure to the details page

--- obj/Debug/net8.0/Blazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d0a6e4d9e5b4cc8b152e85e0bb009340511e812")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+817dd5d5b1f08679fe0fc96f10fcbd1c38e8396d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
